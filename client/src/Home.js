284b409d260a625ea941428e3bfc1589b36ae5fe
philpatt <phillip.s.patterson1@gmail.com>
2018-01-24 13:18:10 -0800

    git pull text

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-  render(){
-    return (
-        <p>This is a home page. You probably ought to write something here</p>
-      );
-  }
-}
+	constructor(props){
+		super(props)
+		this.state={
+				zipCode: '',
+				weather:''
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(e){
+    this.setState({zipCode: e.target.value}, () => {
+    console.log('zipcode', this.state.zipCode);
+
+    	})
+	}
+	handleSubmit(e){
+		e.preventDefault(e);
+		var base = this
+		let zipCode = this.state.zipCode;
+  		let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip='+ zipCode +',us&appid=052f26926ae9784c2d677ca7bc5dec98'
+  		fetch(weatherApi)
+  		.then((response) =>{
+  			console.log('response returned is', response);
+  			return response.json()
+  		}).then((json) => {
+  			console.log('response json is', json);
+
+  			base.setState({ weather: json.name});
+
+
+    		console.log('weather state', this.state.weather);
+
+  		}).catch((ex) => {
+  			console.log('an error occured while parsing!' ,ex);
+  		})
+  	}	
+  	render () {
+
+    	return (
+    		<div>
+    		<Widget weather={ this.state.weather } />
+	    		<form onSubmit={ this.handleSubmit }>
+						<label>
+							<p>Please enter your zip code for the weather: </p>
+							<input type="text"  onChange={this.handleChange} />
+						</label>
+						<input type="submit" value="Get my forecast!" />
+					</form>
+			</div>
+    	)
+	}
 
 export default Home;
